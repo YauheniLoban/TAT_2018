aaf0803e409e8a6f6495f5f3fc5f60f8e140745a
YauheniLoban <zhenya-loban@mail.ru>
2018-03-03 00:18:26 +0300

    Errors fix

--- HomeTask1/HomeTask1/Program.cs
+++ HomeTask1/HomeTask1/Program.cs
@@ -2,38 +2,58 @@
 
 namespace DevTask1
 {
-    class EntryPoint
+    /// <summary> 
+    /// Class MaxNumberOfDuplicates
+    /// Searches for the maximum number of identical items in a row
+    /// </summary> 
+    class MaxNumberOfDuplicates 
     {
         static void Main(string[] args)
         {
-           
-            if (args.Length != 1) //Checking the condition for the argument
+            if (args.Length != 1) // Checking the condition for the argument
             {
                 Console.WriteLine("Wrong number of arguments");
                 return;
             }
-            if (args.Length == 0) //Checking for the presence of elements in a row
+
+            if (args.Length == 0) // Checking for the presence of elements in a row
             {
                 Console.WriteLine("Enter a non-empty string");
                 return;
             }
-            EntryPoint need = new EntryPoint();
-            var commandLineArgument = args[0].ToCharArray();
-            Console.WriteLine(need.MaximumNumberofDuplicates(commandLineArgument));
+
+            MaxNumberOfDuplicates EntryPointObject = new MaxNumberOfDuplicates();
+            Console.WriteLine(EntryPointObject.MaximumNumberofDuplicates(args[0]));
         }
-        //Function for finding the maximum number of repeating identical elements in succession
-        int MaximumNumberofDuplicates(char[] transmittedString) 
+
+
+        /// <summary> 
+        /// Method MaximumNumberofDuplicates 
+        /// Function for finding the maximum number of repeating identical elements in succession
+        /// </summary> 
+        /// <param name="transmittedString">String, which was inputed</param> 
+        /// <returns>The largest number of identical characters in succession</returns>
+        int MaximumNumberofDuplicates(String transmittedString) 
         {
-            int maximumAmount = 0, temp = 0;
+            int maximumAmount = 0, tempMaxNumber = 0;
+
             for (int i = 0; i < transmittedString.Length - 1; i++)
-            {   
-                if (transmittedString[i] == transmittedString[i + 1]) temp++; //Check for a match between the current and the next element
-                else temp = 0; //Assign the counter to zero if the elements do not match
-                if (temp > maximumAmount) //Check if this sequence is longer
+            {
+                if (transmittedString[i] == transmittedString[i + 1])
                 {
-                    maximumAmount = temp;
+                    tempMaxNumber++; // Check for a match between the current and the next element
+                }
+                else
+                {
+                    tempMaxNumber = 0; // Assign the counter to zero if the elements do not match
+                }
+
+                if (tempMaxNumber > maximumAmount) //Check if this sequence is longer
+                {
+                    maximumAmount = tempMaxNumber;
                 }
             }
+
             return maximumAmount + 1;
         }
     }
--- HomeTask1/HomeTask1/Properties/AssemblyInfo.cs
+++ HomeTask1/HomeTask1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HomeTask1.Properties")]
+[assembly: AssemblyTitle("HomeTask1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HomeTask1.Properties")]
+[assembly: AssemblyProduct("HomeTask1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7e32290-6aa1-44e2-a417-25e62d39383d")]
+[assembly: Guid("1943ecb7-1fda-4a8e-ad13-43e6e4ca898d")]
 
 // Version information for an assembly consists of the following four values:
 //
