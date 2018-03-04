28d6076896fdc0fa7dfcd636f170677bfdd1ef50
YauheniLoban <zhenya-loban@mail.ru>
2018-03-04 19:30:03 +0300

    code stele fix

--- HomeTask2/HomeTask2/Program.cs
+++ HomeTask2/HomeTask2/Program.cs
@@ -4,7 +4,7 @@ namespace DevTask2
 {
     /// <summary> 
     /// EntryPointObject
-    ///Gets the string from console and prints a substring of elements with even indices to the console
+    /// Gets the string from console and prints a substring of elements with even indices to the console
     /// </summary> 
     class EntryPoint
     {
@@ -15,7 +15,8 @@ namespace DevTask2
             {
                 Console.WriteLine("Enter a non-empty string");
                 stringEnteredFromConsole = Console.ReadLine();
-            }while (stringEnteredFromConsole.Length == 0);
+            } while (stringEnteredFromConsole.Length == 0);
+
             SubstringOfCharactersWithEvenIndices EvenIndexString = new SubstringOfCharactersWithEvenIndices();
             Console.WriteLine(EvenIndexString.CreateSubstringOfEvenElements(stringEnteredFromConsole));
         }
