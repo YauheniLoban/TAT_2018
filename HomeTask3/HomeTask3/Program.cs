00998913d48d6e256211a89806269cda89188701
YauheniLoban <zhenya-loban@mail.ru>
2018-03-06 10:39:56 +0300

    task_DEV-3

---
+++ HomeTask3/HomeTask3/Program.cs
@@ -0,0 +1,38 @@
+﻿using DevTask3;
+using System;
+namespace Task_Dev3
+{
+    /// <summary> 
+    /// EntryPoint
+    /// We accept from the command line two arguments: 
+    /// 1) the number that needs to be transferred to another system of calculation 
+    /// 2) the foundation of a new system of calculus 
+    /// </summary> 
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            //We check the arguments for the correctness of the input (only numbers can be entered)
+            if(args[0] == string.Empty || args[1] == string.Empty)
+            {
+                Console.WriteLine("You entered an empty string");
+                return;
+
+            }
+            int numberForCheck;
+            if (!Int32.TryParse(args[0], out numberForCheck)||!Int32.TryParse(args[1], out numberForCheck))
+            {
+                Console.WriteLine("You entered not only numbers in the argument string. Please correct the arguments");
+                return ;
+            }
+            if (Convert.ToInt16(args[1]) < 1 || Convert.ToInt16(args[1]) > 20)
+            {
+                Console.WriteLine("You entered the base of the new system not from the allowed range. Please enter another base of the new system ");
+                return ;
+            }
+            translationIntoNewSystem newConvertedString = new translationIntoNewSystem(args);       
+            Console.WriteLine(newConvertedString.CreatingRepresentationOfNumberInNewSystem());
+        }
+
+    }
+}
---
+++ HomeTask3/HomeTask3/translationIntoNewSystem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace DevTask3
+{
+    /// <summary> 
+    /// translationIntoNewSystem
+    /// creates a string that is a translation of a number into another number system
+    /// </summary>
+    public class translationIntoNewSystem
+    {
+        private long numberForConversion;
+        private long foundationOfNewSystem;
+
+        public translationIntoNewSystem(string[] valuesNumberAndBaseOfSystem)
+        {
+            numberForConversion = Convert.ToInt64(valuesNumberAndBaseOfSystem[0]);
+            foundationOfNewSystem = Convert.ToInt64(valuesNumberAndBaseOfSystem[1]);
+        }
+
+        /// <summary> 
+        /// Method CreateSubstringOfEvenElements
+        /// A method that creates a string that is a representation of a number in the new number system
+        /// </summary> 
+        /// <returns>valueInNewSystem</returns>
+        public string CreatingRepresentationOfNumberInNewSystem()
+        {
+            StringBuilder valueInNewSystem = new StringBuilder();
+            long nextNumberToAdd;
+            do
+            {
+                nextNumberToAdd = numberForConversion % foundationOfNewSystem;
+                valueInNewSystem.Append(ConvertingFigureToRepresentationInAnotherSystem(nextNumberToAdd));
+                numberForConversion = numberForConversion / foundationOfNewSystem;
+            } while (0 != numberForConversion);
+            string ConvertedString;
+            ConvertedString = valueInNewSystem.ToString();
+
+            return reverseString(ConvertedString);
+        }
+        /// <summary> 
+        /// ConvertingFigureToRepresentationInAnotherSystem
+        /// Function for converting numbers to a new number system
+        /// </summary> 
+        /// <param name="digitForTranslation">number to convert to a new system</param> 
+        /// <returns>figureInNewSystem</returns>
+        private string ConvertingFigureToRepresentationInAnotherSystem (long digitForTranslation)
+        {
+            char figureInNewSystem;
+            int index;
+            index = Convert.ToInt16(digitForTranslation);
+            var possibleValuesOfElementsInNewView = "0123456789ABCDEFGHIJ";
+            figureInNewSystem = possibleValuesOfElementsInNewView[index];
+            return Convert.ToString(figureInNewSystem);
+        }
+        /// <summary> 
+        /// reverseString
+        /// Function to create a string of characters in which go in reverse order
+        /// </summary> 
+        /// <param name="stringToReverse">The string you need to revers</param> 
+        /// <returns>reversedString</returns>
+        private string reverseString(string stringToReverse)
+           {
+                char[] reversedString = stringToReverse.ToCharArray();
+                Array.Reverse(reversedString);
+                return new string(reversedString);
+            }
+    }
+}
+
+
+
