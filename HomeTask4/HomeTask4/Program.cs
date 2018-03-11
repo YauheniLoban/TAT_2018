ee417ab54ae9ef8e988ef8e6790cb079012de468
YauheniLoban <zhenya-loban@mail.ru>
2018-03-12 01:15:04 +0300

    Task 4

---
+++ HomeTask4/HomeTask4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeTask4
+{
+    /// <summary>
+    /// We accept from the comand line the path to the file
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ParserForXmlFile str = new ParserForXmlFile(args[0]);
+                str.OutputtingDataToConsole();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("you entered incorrect arguments");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("you entered the wrong path to the file");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("you entered incorrect arguments");
+            }
+        }
+    }
+}
