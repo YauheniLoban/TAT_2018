85214f44f58c145ae0d02e23b9b31099c76fbbab
YauheniLoban <zhenya-loban@mail.ru>
2018-04-19 20:34:04 +0300

    1st solution

---
+++ Task_19_04_2018/ConsoleApplication1/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_19_04_2018
+{
+    public delegate double MethodOfCalculation(List<double> x, List<double> y);
+
+    /// <summary>  
+    /// using various functions to calculate distance, advanced functions through delegates
+    /// </summary> 
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            List<double> x = new List<double> { 0,0};
+            List<double> y = new List<double> { 3,4 };
+            Сalculator comands = new Сalculator();
+            Сoordinates pairOfCoordinates = new Сoordinates(x, y, comands.DistanceInRectangularCoordinates);
+            Console.WriteLine("Resalt:" + pairOfCoordinates.GetDistens());
+            x.Add(0);
+            y.Add(10);
+            pairOfCoordinates.point1 = x;
+            pairOfCoordinates.point2 = y;
+            Console.WriteLine("Resalt:" + pairOfCoordinates.GetDistens());
+            pairOfCoordinates.counter = comands.DistanceInSomeCoordinates;
+            Console.WriteLine("Resalt:" + pairOfCoordinates.GetDistens());
+        }
+    }
+}
---
+++ Task_19_04_2018/ConsoleApplication1/Сalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_19_04_2018
+{
+    /// <summary>  
+    /// class containing various ways of calculating the distance between two points
+    /// </summary> 
+    class Сalculator
+    {
+        /// <summary>  
+        /// Calculate Distance in rectangular coordinates
+        /// </summary> 
+        public double DistanceInRectangularCoordinates(List<double> x, List<double> y)
+        {
+            double distance = 0;
+            if(x.Count==y.Count)
+            {
+                for(int i=0;i<x.Count;i++)
+                {
+                    distance += Math.Pow(x[i] - y[i],2);
+                }
+                distance = Math.Sqrt(distance);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return distance;
+        }
+
+        /// <summary>  
+        /// Calculate Distance in some coordinates
+        /// </summary> 
+        public double DistanceInSomeCoordinates(List<double> x, List<double> y)
+        {
+            double distance = 0;
+            if (x.Count == y.Count)
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    distance += Math.Pow(x[i] + y[i], 2);
+                }
+                distance = Math.Sqrt(distance);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return distance;
+        }
+
+    }
+}
