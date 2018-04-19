0418b6d00c08579397ffac5f558d9c3545880d5d
YauheniLoban <zhenya-loban@mail.ru>
2018-04-19 20:38:32 +0300

    all ok

---
+++ Task_19_04_2018/ConsoleApplication1/Сoordinates.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Task_19_04_2018
+{
+    /// <summary>  
+    /// Two points in some coordinate system and 
+    /// </summary> 
+    class Сoordinates
+    {
+        public List<double> point1 = new List<double>();
+        public List<double> point2 = new List<double>();
+        public MethodOfCalculation counter { get; set; }
+
+        public Сoordinates(List<double> x, List<double> y, MethodOfCalculation _counter)
+        {
+            point1 = x;
+            point2 = y;
+            counter = _counter;
+        }
+
+        /// <summary>  
+        /// Calculate the distance between two points
+        /// </summary> 
+        public double GetDistens()
+         {
+            return counter(point1, point2);
+         }
+    }
+}
